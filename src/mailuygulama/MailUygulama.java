f1c321bf8ba4cb6e7ca74155000abe7f5bf3cbef
Mehmet KAYKISIZ <m.kaykisiz@gmail.com>
2014-01-04 14:44:44 +0200

    Mail Send App
    This app is send to mail with ssl in java

---
+++ src/mailuygulama/MailUygulama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mailuygulama;
+
+/**
+ *
+ * @author mehmet
+ */
+public class MailUygulama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     AnaPencere ap=new AnaPencere();
+     ap.setVisible(true);
+    }
+}
